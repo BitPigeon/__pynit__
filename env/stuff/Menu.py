35edbd0d28a92914df167d7ac6ca4e9bafe80dbc
Bit Pigeon <84301435+BitPigeon@users.noreply.github.com>
2023-03-31 07:45:07 -0700

    Update Menu.py

--- env/stuff/Menu.py
+++ env/stuff/Menu.py
@@ -1,2 +1,13 @@
+from tkinter import *
+from tkinter.ttk import *
 
-
+class menu(Menubar):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.parent.config(menu=self)
+        self.file_menu = Menubar(root, tearoff=0)
+        self.file_menu.add_seperator()
+        self.file_menu.add_command("Close", command=self.close_main_window)
+    def close_main_window(self):
+        self.parent.destroy()
